c7becfa165f226888b991ba9b1bf8eb0208b8dff
Tom Goskar <tom@goskar.com>
2015-10-22 10:36:32 +0100

    Change layout to use single thumbnail

--- exhibit-builder/exhibits/browse.php
+++ exhibit-builder/exhibits/browse.php
@@ -6,23 +6,21 @@
 <div class="container">
     <?php if (count($exhibits) > 0): ?>
         <?php $exhibitCount = 0; ?>
-    
+
         <?php foreach (loop('exhibit') as $exhibit): ?>
             <div class="content-block exhibit">
                 <?php $exhibitCount++; ?>
                 <h1><?php echo link_to_exhibit(); ?></h1>
-                <div class="items-list slider exhibit-slider">
-                    <?php print_r(sckls_exhibit_builder_get_images($exhibit)); ?>
-                </div>
+                <?php if ($exhibitImage = record_image($exhibit, 'square_thumbnail')): ?>
+                    <?php echo exhibit_builder_link_to_exhibit($exhibit, $exhibitImage, array('class' => 'exhibit-browse-image')); ?>
+                <?php endif; ?>
                 <?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
-                    <hr>
-                    <?php echo $exhibitDescription; ?>
-                    <?php echo link_to_exhibit('View this exhibit', array('class' => 'link-to-exhibit')); ?>
+                <div class="description"><?php echo $exhibitDescription; ?></div>
                 <?php endif; ?>
             </div>
         <?php endforeach; ?>
         <?php echo pagination_links(); ?>
-    
+
     <?php else: ?>
         <div class="content-block">
             <h1>Browse all exhibits</h1>
