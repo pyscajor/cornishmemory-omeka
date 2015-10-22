216ffbb7b62baeb2cba42c010c6ef3c82e50be22
Tom Goskar <tom@goskar.com>
2015-10-22 10:37:00 +0100

    Add Cookie Consent code

--- common/header.php
+++ common/header.php
@@ -64,6 +64,14 @@
         }
     </style>
     </style>
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+<script type="text/javascript">
+    window.cookieconsent_options = {"message":"This website uses cookies to ensure you get the best experience on our website","dismiss":"Got it!","learnMore":"More info","link":"http://cornishmemory.com/privacy","theme":"dark-bottom"};
+</script>
+
+<script type="text/javascript" src="//s3.amazonaws.com/cc.silktide.com/cookieconsent.latest.min.js"></script>
+<!-- End Cookie Consent plugin -->
+
 </head>
 <?php echo body_tag(array('id' => @$bodyid, 'class' => @$bodyclass)); ?>
 <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
