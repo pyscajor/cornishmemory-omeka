1fd3a41c11e7c7fa2d6e80ec8df5cdce5b2feae3
Tom Goskar <tom@goskar.com>
2015-11-03 13:15:59 +0000

    Simple custom 404 page

---
+++ error/404.php
@@ -0,0 +1,9 @@
+<div id="primary">
+
+<h1>Oops!</h1>
+<img src="/cm-images/cm-logo.png" alt="cornishmemory.com" />
+<div id="primary-content">
+	<p>Sorry, this page doesn't exist. Try visiting the <a href="http://cornishmemory.com">cornishmemory.com</a> homepage.</p>
+</div>
+
+</div><!-- end primary -->
