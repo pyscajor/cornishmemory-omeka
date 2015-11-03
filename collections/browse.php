e80b1e07564a772bdb93cd7a97fa0ef739d0fa26
Tom Goskar <tom@goskar.com>
2015-11-03 13:14:30 +0000

    Add logic for search filters, totals

--- collections/browse.php
+++ collections/browse.php
@@ -7,27 +7,27 @@
 
     <?php if ($collections): ?>
         <?php foreach (loop('collections') as $collection): ?>
-        <div class="content-block browse-page">        
-            <h1><?php echo link_to_collection(); ?></h1>
+        <div class="content-block browse-page">
+            <h1><?php echo link_to_collection(); ?> <small>(<?php echo metadata('collection', 'total_items'); ?> Items)</small></h1>
             <?php
                 $items = get_records('Item', array('collection'=>$collection->id), 8);
                 set_loop_records('items', $items);
                 if (has_loop_records('items')): ?>
-    
+
                 <div class="items-list slider">
                 <?php foreach (loop('items') as $item): ?>
                     <?php $image = $item->Files; ?>
                     <?php if ($image): ?>
                     <div class="item">
 						<?php
-							
+
 						echo '  <a href="' . record_url($item, null, true) . '">';
 						echo '    <div class="overlay"></div>';
 								if (metadata('item', 'has thumbnail')):
 							echo '<div style="background-image: url(' . file_display_url($image[0], 'fullsize') . ');" class="img"></div>';
 							else:
 							echo '<div style="background-image: url(/themes/cornishmemory/images/fallback-video.png);" class="img"></div>';
-								endif;					                          
+								endif;
 						echo '    <span class="title">' . metadata('item', array('Dublin Core', 'Title')) . '</span>';
 						echo '  </a>';
 											                                ?>
@@ -36,12 +36,12 @@
                 <?php endforeach; ?>
                 </div>
             <?php endif; //endif; ?>
-    
+
             <?php if (metadata('collection', array('Dublin Core', 'Description'))): ?>
                 <hr>
                 <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet'=>150))); ?>
                 <?php echo link_to_collection('View this collection', array('class' => 'link-to-exhibit')); ?>
-            <?php endif; ?>        
+            <?php endif; ?>
         </div>
         <?php endforeach; ?>
     <?php else : ?>
